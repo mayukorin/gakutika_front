57bc7f66f96f474022e2a71e61283d85176da241
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2022-03-12 23:31:37 +0900

    feat: add user_and_company_create_form to companyIndex

--- src/components/organisms/CompanyIndex.vue
+++ src/components/organisms/CompanyIndex.vue
@@ -3,6 +3,21 @@
     <ProgressCircular v-show="loading" />
     <div v-show="!loading">
       <h3 class="success--text mr-3 ml-1 title">面接を受ける企業一覧</h3>
+      <form-dialog :propsFormShowFlag.sync="userAndCompanyFormShowFlag">
+        <Button
+          :class-string="'success ml-1 mt-2 mb-2'"
+          slot="btn"
+          @click="userAndCompanyFormShowFlag = true"
+        >
+          面接を受ける企業追加
+        </Button>
+        <userAndCompanyCreateCard
+          slot="formCard"
+          store-action-name="userAndCompanies/fetchuserAndCompanies"
+          create-store-action-name="createUserAndCompany"
+          @created="userAndCompanyFormShowFlag = false"
+        />
+      </form-dialog>
       <CompanyList
         :user-and-companies="userAndCompanies"
         store-action-name="userAndCompanies/fetchuserAndCompanies"
@@ -13,16 +28,23 @@
 <script>
 import CompanyList from "@/components/organisms/CompanyList";
 import ProgressCircular from "@/components/atoms/ProgressCircular.vue";
+import UserAndCompanyCreateCard from "@/components/organisms/UserAndCompanyCreateCard";
+import FormDialog from "@/components/organisms/FormDialog";
+import Button from "@/components/atoms/Button.vue";
 
 export default {
   name: "CompanyIndex",
   components: {
     CompanyList,
     ProgressCircular,
+    UserAndCompanyCreateCard,
+    FormDialog,
+    Button,
   },
   data() {
     return {
       loading: false,
+      userAndCompanyFormShowFlag: false,
     };
   },
   computed: {
